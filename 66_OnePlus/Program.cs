9a8dc056dec5f31312eff34437214e67e29c8daf
Javlon Alimov <javlonalimov1@gmail.com>
2024-10-15 14:25:49 +0500

    1

---
+++ 66_OnePlus/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+
+        string result = string.Join(string.Empty, digits);
+
+        int numbers = int.Parse(result);
+
+        numbers += 1;
+
+        string numberString = numbers.ToString();
+
+
+        int[] resultArray = numberString.Select(digit => int.Parse(digit.ToString())).ToArray();
+        return resultArray;
+    }
+}
